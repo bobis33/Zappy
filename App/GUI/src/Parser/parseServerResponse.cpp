c19dbd7a265173265f0596f2fc61241ec500d584
MASINA Elliot <elliot.masina@epitech.eu>
2024-06-04 21:46:23 +0200

    [REFACTOR] GUI: clean code

--- App/GUI/src/Parser/parseServerResponse.cpp
+++ App/GUI/src/Parser/parseServerResponse.cpp
@@ -27,31 +27,16 @@ void gui::Parser::processData(const std::vector<std::string>& data, Gui &gui)
 
 gui::Tile gui::Parser::parseTileContent(std::string &tileContent)
 {
-
     for (char & index : tileContent) {
         if (index == 0) {
             index = ' ';
         }
     }
     std::istringstream iss(tileContent);
-    std::vector<unsigned int> values((std::istream_iterator<int>(iss)), std::istream_iterator<int>());
-
-    if (values.size() == 0) {
-
-    }
+    std::vector<unsigned int> values((std::istream_iterator<int>(iss)), std::istream_iterator<int>())
     if (values.size() != 9) {
         throw std::runtime_error("Invalid tile content");
     }
-    std::cout << "values0: " << values.at(0) << " " << '\n';
-    std::cout << "values1: " << values.at(1) << " " << '\n';
-    std::cout << "values2: " << values.at(2) << " " << '\n';
-    std::cout << "values3: " << values.at(3) << " " << '\n';
-    std::cout << "values4: " << values.at(4) << " " << '\n';
-    std::cout << "values5: " << values.at(5) << " " << '\n';
-    std::cout << "values6: " << values.at(6) << " " << '\n';
-    std::cout << "values7: " << values.at(7) << " " << '\n';
-    std::cout << "values8: " << values.at(8) << " " << '\n';
-
     return {
         {
             {Resource::Type::FOOD, values.at(2)},
