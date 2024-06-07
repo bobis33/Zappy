ded941f42e0e268538784810c39a71c1de8a4273
MASINA Elliot <elliot.masina@epitech.eu>
2024-06-07 16:03:40 +0200

    [FIX] GUI: assets

--- App/GUI/include/GUI/Player.hpp
+++ App/GUI/include/GUI/Player.hpp
@@ -23,20 +23,31 @@ namespace gui {
                 NONE
             };
 
+            enum class Direction {
+                UP,
+                DOWN,
+                LEFT,
+                RIGHT
+            };
+
             Action getAction() const { return m_action; };
+            Direction getDirection() const { return m_direction; };
             Inventory& getInventory() { return m_inventory; };
             Position& getPosition() { return m_position; };
             unsigned int getLevel() const { return m_level; };
 
             void setAction(const Action action) { m_action = action; };
+            void setDirection(const Direction direction) { m_direction = direction; };
 
             void levelUp() { m_level++; };
 
         private:
 
+            bool m_isAlive{true};
             Action m_action{Action::NONE};
             Inventory m_inventory;
             Position m_position;
+            Direction m_direction{Direction::UP};
             unsigned int m_level{1};
 
     }; // class Player
