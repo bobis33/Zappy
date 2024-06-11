42eefd6bb499decae11251af70886f41fb452a94
ShrimpPR <tristan.le-du@epitech.eu>
2024-06-11 14:49:00 +0200

    Reapply "[FEAT] : display items on the map -> might change a lot in about an hour"
    This reverts commit 5feeacfcbaecd55728cdb836947d6803d4463a96.

--- App/GUI/lib/shared/Renderer/SFML/src/SFML.cpp
+++ App/GUI/lib/shared/Renderer/SFML/src/SFML.cpp
@@ -33,11 +33,60 @@ void gui::SFML::init(const std::string &name, std::pair<const unsigned int,const
         throw std::runtime_error("Failed to load texture");
     }
     addTexture(texture, "forest");
+    if (!texture.loadFromFile("assets/textures/resources/wine.png")) {
+        throw std::runtime_error("Failed to load texture");
+    }
+    addTexture(texture, "food");
+    if (!texture.loadFromFile("assets/textures/resources/stone_1.png")) {
+        throw std::runtime_error("Failed to load texture");
+    }
+    addTexture(texture, "linemate");
+    if (!texture.loadFromFile("assets/textures/resources/stone_2.png")) {
+        throw std::runtime_error("Failed to load texture");
+    }
+    addTexture(texture, "deraumere");
+    if (!texture.loadFromFile("assets/textures/resources/stone_3.png")) {
+        throw std::runtime_error("Failed to load texture");
+    }
+    addTexture(texture, "sibur");
+    if (!texture.loadFromFile("assets/textures/resources/stone_4.png")) {
+        throw std::runtime_error("Failed to load texture");
+    }
+    addTexture(texture, "mendiane");
+    if (!texture.loadFromFile("assets/textures/resources/stone_5.png")) {
+        throw std::runtime_error("Failed to load texture");
+    }
+    addTexture(texture, "phiras");
+    if (!texture.loadFromFile("assets/textures/resources/stone_6.png")) {
+        throw std::runtime_error("Failed to load texture");
+    }
+    addTexture(texture, "thystame");
     sf::Sprite sprite;
     sf::IntRect textureRect(100, 85, 40, 40);
     sprite.setTexture(getTextures().at(0).first);
     sprite.setTextureRect(textureRect);
     addSprite(sprite, "forest");
+    sprite.setTexture(getTextures().at(1).first);
+    sprite.setTextureRect(sf::IntRect(0, 0, 10, 31));
+    addSprite(sprite, "food");
+    sprite.setTexture(getTextures().at(2).first);
+    sprite.setTextureRect(sf::IntRect(0, 0, 18, 30));
+    addSprite(sprite, "linemate");
+    sprite.setTexture(getTextures().at(3).first);
+    sprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
+    addSprite(sprite, "deraumere");
+    sprite.setTexture(getTextures().at(4).first);
+    sprite.setTextureRect(sf::IntRect(0, 0, 30, 32));
+    addSprite(sprite, "sibur");
+    sprite.setTexture(getTextures().at(5).first);
+    sprite.setTextureRect(sf::IntRect(0, 0, 32, 30));
+    addSprite(sprite, "mendiane");
+    sprite.setTexture(getTextures().at(6).first);
+    sprite.setTextureRect(sf::IntRect(0, 0, 28, 30));
+    addSprite(sprite, "phiras");
+    sprite.setTexture(getTextures().at(7).first);
+    sprite.setTextureRect(sf::IntRect(0, 0, 28, 30));
+    addSprite(sprite, "thystame");
 }
 
 bool gui::SFML::checkConnection(sf::Clock clock)
@@ -72,13 +121,46 @@ void gui::SFML::render(Map &map)
         sprite.first.setScale(scaleX, scaleY);
         i++;
     }
+    for (int i = 1; i < getSprites().size(); i++) {
+        getSprites().at(i).first.setScale(0.8, 0.8); // Tmp -> change the scale to match with the density of the item
+    }
     for (auto& row : map.getTiles()) {
         for (auto& tile : row) {
-            getSprites().at(0).first.setPosition(
+            getSprites().at(0).first.setPosition(                           // la map
                 static_cast<float>(tile.getPosition().x) * (getSprites().at(0).first.getGlobalBounds().width),
                 static_cast<float>(tile.getPosition().y) * (getSprites().at(0).first.getGlobalBounds().height)
             );
-            m_window.draw(getSprites().at(0).first);
+            getSprites().at(1).first.setPosition(                           // le pinard
+                static_cast<float>(tile.getPosition().x) * (getSprites().at(0).first.getGlobalBounds().width) + 48,
+                static_cast<float>(tile.getPosition().y) * (getSprites().at(0).first.getGlobalBounds().height) + 43
+            );
+            getSprites().at(2).first.setPosition(                           // le petit vert
+                static_cast<float>(tile.getPosition().x) * (getSprites().at(0).first.getGlobalBounds().width) + 20,
+                static_cast<float>(tile.getPosition().y) * (getSprites().at(0).first.getGlobalBounds().height) + 15
+            );
+            getSprites().at(3).first.setPosition(                           // le rubis
+                static_cast<float>(tile.getPosition().x) * (getSprites().at(0).first.getGlobalBounds().width) + 40,
+                static_cast<float>(tile.getPosition().y) * (getSprites().at(0).first.getGlobalBounds().height) + 15
+            );
+            getSprites().at(4).first.setPosition(                           // le gros vert
+                static_cast<float>(tile.getPosition().x) * (getSprites().at(0).first.getGlobalBounds().width) + 70,
+                static_cast<float>(tile.getPosition().y) * (getSprites().at(0).first.getGlobalBounds().height) + 15
+            );
+            getSprites().at(5).first.setPosition(                           // le diamant
+                static_cast<float>(tile.getPosition().x) * (getSprites().at(0).first.getGlobalBounds().width) + 15,
+                static_cast<float>(tile.getPosition().y) * (getSprites().at(0).first.getGlobalBounds().height) + 70
+            );
+            getSprites().at(6).first.setPosition(                           // l'ambre
+                static_cast<float>(tile.getPosition().x) * (getSprites().at(0).first.getGlobalBounds().width) + 42,
+                static_cast<float>(tile.getPosition().y) * (getSprites().at(0).first.getGlobalBounds().height) + 70
+            );
+            getSprites().at(7).first.setPosition(                           // l'amethyste
+                static_cast<float>(tile.getPosition().x) * (getSprites().at(0).first.getGlobalBounds().width) + 70,
+                static_cast<float>(tile.getPosition().y) * (getSprites().at(0).first.getGlobalBounds().height) + 70
+            );
+            for (const auto& sprite : getSprites()) {
+                m_window.draw(sprite.first);
+            }
         }
     }
     m_window.display();
