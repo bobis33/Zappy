6abaa760e22d15826e94c69d28421563f113240b
MASINA Elliot <elliot.masina@epitech.eu>
2024-06-21 16:28:26 +0200

    [FEAT] : AI Look command in progress.
    [FEAT] : AI Look command in progress.

--- App/Server/src/input/client/AI/cmd_look.c
+++ App/Server/src/input/client/AI/cmd_look.c
@@ -6,6 +6,7 @@
 */
 
 #include "Server/cmd_ai_client.h"
+#include <string.h>
 
 static void look_west(player_t *player, game_t *game)
 {
@@ -25,10 +26,60 @@ static void look_south(player_t *player, game_t *game)
     print_msg(player->fd_client, "ok\n");
 }
 
+const char *resource_type_to_string(resource_type_t type)
+{
+    switch (type) {
+        case FOOD:
+            return "food";
+        case LINEMATE:
+            return "linemate";
+        case DERAUMERE:
+            return "deraumere";
+        case SIBUR:
+            return "sibur";
+        case MENDIANE:
+            return "mendiane";
+        case PHIRAS:
+            return "phiras";
+        case THYSTAME:
+            return "thystame";
+        default:
+            return "unknown";
+    }
+}
+
 static void look_north(player_t *player, game_t *game)
 {
-    (void)game;
-    print_msg(player->fd_client, "ok\n");
+    print_msg(player->fd_client, "[");
+    int first_tile = 1;
+    for (int i = 0; i <= player->level; ++i) {
+        for (int j = -i; j <= i; ++j) {
+            int tile_y = (player->position.y - i + game->map->height) % game->map->height;
+            int tile_x = (player->position.x + j + game->map->width) % game->map->width;
+            tile_t *tile = &game->map->tiles[tile_y][tile_x];
+            if (!first_tile) {
+                print_msg(player->fd_client, ", ");
+            }
+            int first_resource = 1;
+            char tile_contents[1024] = {0};
+            printf("tile is (%d, %d)\n", tile_x, tile_y);
+            for (int k = 0; k < COUNT; ++k) {
+                if (tile->resources[k].quantity > 0) {
+                    if (!first_resource) {
+                        strcat(tile_contents, " ");
+                    }
+                    strcat(tile_contents, resource_type_to_string(tile->resources[k].type));
+                    first_resource = 0;
+                }
+            }
+            if (strlen(tile_contents) == 0) {
+                strcat(tile_contents, "");
+            }
+            print_msg(player->fd_client, tile_contents);
+            first_tile = 0;
+        }
+    }
+    print_msg(player->fd_client, "]\n");
 }
 
 void cmd_look(player_t *player, game_t *game)
